a6364a7daeb2a89e1601d66f81c22709e4da88f5
trox0day <122801000+Tr0x123@users.noreply.github.com>
2025-09-26 20:58:28 +0200

    Update OOP01.java
    Popravljen ispis proseka

--- OOP01.java
+++ OOP01.java
@@ -16,41 +16,18 @@ public class OOP01 {
         System.out.print("Unesite trecu ocenu: ");
         int ocena3 = s.nextInt();
         
-        int prosek = (ocena1 + ocena2 + ocena3) / 3;
+        double prosek = (ocena1 + ocena2 + ocena3) / 3.0;
         
         if (prosek < 2) {
-        
             System.out.println("Nedovoljan uspeh");
-            
         } else if (prosek < 3.5) {
-            
             System.out.println("Dobar uspeh");
-            
         } else if (prosek < 4.5) {
-            
             System.out.println("Vrlo dobar uspeh");
-            
         } else {
-            
             System.out.println("Odlican uspeh!");
-            
         }
         
-        
+        System.out.printf("Prosek je: %.2f\n", prosek);
     }
-    
 }
-
-
-/**
- * 
- * Unesi 3 ocene sa tastature
- * Izracunaj prosek
- * Na osnovu proseka ispisi poruku
- * 
- * Prosek manji od 2 -> Nedovoljan uspeh
- * 2 - 3.5 -> Dobar uspeh
- * 3.5 - 4.5 -> Vrlo dobar uspeh
- * 4.5 i vise -> Odlican uspeh
- * 
- */
