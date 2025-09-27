66ccb326a35dcb89eca9c1bd748e30dd06337279
trox0day <122801000+Tr0x123@users.noreply.github.com>
2025-09-28 01:14:06 +0200

    Add files via upload

---
+++ OOP11.java
@@ -0,0 +1,26 @@
+package oop11;
+
+public class OOP11 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        int z = x + y;
+        
+        System.out.println(z);
+        
+        z = x - y;
+        System.out.println("Vrednost posle promene: " + z + " " +y);
+        
+        String rec = "Vrednost posle promene: ";
+        
+        System.out.println(rec + z + " " + y);
+        System.out.println(100 + 50);
+        double brojC = 13;
+        System.out.println(brojC);
+        
+        boolean uslov = true;
+        System.out.println(uslov);
+        
+    }
+    
+}
