33f6d981957b7fff2b1a3acdb16ea1d23ec028a1
trox <122801000+tr0x1337@users.noreply.github.com>
2025-09-28 22:15:26 +0200

    Add files via upload

---
+++ Zdravo.java
@@ -0,0 +1,23 @@
+package com.mycompany.zdravo;
+import java.util.*;
+
+public class Zdravo {
+
+    public static void main(String[] args) 
+    {
+        Scanner tastatura = new Scanner(System.in);
+        
+        System.out.print("Unesite vase ime: ");
+        String ime = tastatura.nextLine();
+        
+        System.out.print("Koliko imate godina: ");
+        int god = tastatura.nextInt();
+        
+        System.out.println("-----------------------");
+        System.out.println("ZELIMO VAM DOBRODOSLICU");
+        System.out.println("-----------------------");
+        
+        System.out.println("Zdravo " + ime + "!");
+        System.out.println("Imate " + god + " godina.");
+    }
+}
