b7737d3cfec9d84a62e9cf2a7b5dfa671bf5ed14
trox <122801000+tr0x1337@users.noreply.github.com>
2025-09-28 08:59:07 +0200

    Update OOP14.java
    Promene :
    
    - Ako se unese exit izlazi se iz programa
    - Optimizovan unos u petlji

--- OOP14.java
+++ OOP14.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class OOP14 {
     public static void main(String[] args) {
-        Scanner kamatna_stopa = new Scanner(System.in);
-        
-        System.out.print("Unesite nesto (Ctrl+Z ili Ctrl+D za kraj): ");
-        while(kamatna_stopa.hasNextLong()) {
-            String token = kamatna_stopa.next();
-            System.out.println("Procitan token: " + token);
-            System.out.println("");
-            System.out.print("Unesite nesto (Ctrl+Z ili Ctrl+D za kraj): ");
-        }
-        
-        kamatna_stopa.close();
-        
-    }
-    
-    
-}
- 
+        Scanner s = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Unesite nesto (exit za kraj): ");
+            String token = s.next();
+
+            if (token.equalsIgnoreCase("exit")) {
+                System.out.println("Kraj programa.");
+                break;
+            }
+
+            System.out.println("Procitan token: " + token);
+            System.out.println();
+        }
+
+        s.close();
+    }
+}
