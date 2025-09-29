ae2857de23fcecca64806814904030741b5579bc
trox <122801000+tr0x1337@users.noreply.github.com>
2025-09-29 10:18:31 +0200

    Add files via upload
    Obican primer for petlje

---
+++ OOP15.java
@@ -0,0 +1,22 @@
+package oop15;
+
+import java.util.Scanner;
+
+public class OOP15 {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        for(int i = 0; i < 10; i++) {
+            
+            System.out.print("Unesi broj: ");
+            int broj = s.nextInt();
+            s.nextLine();
+            
+        }
+        
+        
+    }
+    
+}
